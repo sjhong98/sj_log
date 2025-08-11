4dc11495942247c98abefe7818d4f8338db80f7c
seungjaehongibrdos <162073073+seungjaehongibrdos@users.noreply.github.com>
2025-08-11 11:12:05 +0900

    devLog list ordered desc by title

--- actions/dev/group/getPostListByGroupPk.ts
+++ actions/dev/group/getPostListByGroupPk.ts
@@ -2,12 +2,13 @@
 
 import db from '@/supabase'
 import { devLog } from '@/supabase/schema'
-import { eq } from 'drizzle-orm'
+import { desc, eq } from 'drizzle-orm'
 
 export default async function getPostListByGroupPk(groupPk: number) {
   const postList = await db
     .select()
     .from(devLog)
     .where(eq(devLog.groupPk, groupPk))
+    .orderBy(desc(devLog.title))
   return postList
 }
